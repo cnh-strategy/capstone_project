4bcb5341eab0d3a8816fe275da1feee9618315bf
chanhee <lchanhee92@gmail.com>
2025-10-25 15:51:35 +0000

    cleanup: 불필요한 파일들 삭제, 핵심 파일만 유지

--- demos/debate_ver2/core/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def weighted_mean_std(values, weights):
-    """가중 평균과 표준편차 계산"""
-    weights = np.array(weights)
-    weights /= np.sum(weights)
-    mean = np.sum(values * weights)
-    std = np.sqrt(np.sum(weights * (values - mean) ** 2))
-    return mean, std
