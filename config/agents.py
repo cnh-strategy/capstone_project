1c716a0dcb9e6814d839380a33472c3ad9eb13fb
yegee_SPOTV <ygkim@spotv.net>
2025-12-07 07:40:35 +0900

    (CCEP_MACRO_63.63) 베이스라인 기준 피쳐/하이퍼파람 수정

--- config/agents.py
+++ config/agents.py
@@ -134,18 +134,18 @@ agents_info = {
 
         # 모델 하이퍼파라미터
         "hidden_dims": [128, 64, 32],   # LSTM 3개 층 히든 사이즈
-        "dropout_rates": [0.3, 0.3, 0.2], # 각 층별 Dropout 비율
+        "dropout_rates": [0.1, 0.1, 0.1], # 각 층별 Dropout 비율
         "window_size": 40,              # 시계열 윈도우 크기
         "epochs": 60,
         "patience": 10,
-        "learning_rate": 0.0005,
-        "batch_size": 16,
+        "learning_rate": 0.007,
+        "batch_size": 32,
 
         # 설정 및 기타
         "interval": "1d",
-        "x_scaler": "StandardScaler",
+        "x_scaler": "RobustScaler",
         "y_scaler": "MinMaxScaler",     # 타겟은 -1 ~ 1 범위로 스케일링
-        "loss_fn": "L1Loss",            # L1 Loss (MAE) 사용
+        "loss_fn": "HuberLoss",            # L1 Loss (MAE) 사용
 
         # Debate 관련 파라미터
         "gamma": 0.5,
